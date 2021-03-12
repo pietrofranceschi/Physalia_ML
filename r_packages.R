c7b35e3a535bc7bb5880ece5215b583d235f8d28
filippo <filippo.biscarini@gmail.com>
2021-03-12 14:23:20 +0100

    updated list of r packages

--- r_packages.R
+++ r_packages.R
@@ -2,18 +2,26 @@
 
 ## install libraries
 install.packages("gbm")
+install.packages("broom")
+install.packages("purrr")
 install.packages("glmnet")
 install.packages("ggplot2")
 install.packages("tidyverse")
 install.packages("tidymodels")
 install.packages("data.table")
+install.packages("FactoMineR")
+install.packages("factoextra")
 install.packages("randomForest")
 
 ## load libraries
 library("gbm")
+library("broom")
+library("purrr")
 library("glmnet")
 library("ggplot2")
 library("tidyverse")
 library("tidymodels")
 library("data.table")
+library("FactoMineR")
+library("factoextra")
 library("randomForest")
