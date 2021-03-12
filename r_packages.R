f261f597f47efb038db8fa5da1d8943669da8007
filippo <filippo.biscarini@gmail.com>
2021-03-12 14:03:11 +0100

    modified list of packages to install

--- r_packages.R
+++ r_packages.R
@@ -1,5 +1,15 @@
 ## packages to install
 
+## install libraries
+install.packages("gbm")
+install.packages("glmnet")
+install.packages("ggplot2")
+install.packages("tidyverse")
+install.packages("tidymodels")
+install.packages("data.table")
+install.packages("randomForest")
+
+## load libraries
 library("gbm")
 library("glmnet")
 library("ggplot2")
